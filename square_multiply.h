2b2c07094f267960970878ad2def778281af0e33
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-05 13:03:59 +0100

    square and multiply

---
+++ square_multiply.h
@@ -0,0 +1,7 @@
+#ifndef __SQUARE_MULTIPLY__
+#define __SQUARE_MULTIPLY__
+#include <gmp.h>
+
+unsigned  int square_and_multiply(const mpz_t x,  mpz_t e, const mpz_t n);
+
+#endif /*__SQUARE_MULTIPLY__*/
