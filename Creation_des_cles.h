75c436f5c91bb194af0cda208c6ec77abdcf0f66
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-05 13:04:21 +0100

    creation des cles

---
+++ Creation_des_cles.h
@@ -0,0 +1,13 @@
+#ifndef __CREATION_DES_CLES__
+#define __CREATION_DES_CLES__
+#include <gmp.h>
+
+
+unsigned int generer_cle_publique_N(const unsigned int p, const unsigned int q);
+//unsigned int deduire_cle_secrete_d(unsigned int p, unsigned int q, unsigned int e);
+unsigned int generer_cle_publique_e(const unsigned int p, const unsigned int q);
+int pgcd(int a, int b);
+
+
+
+#endif /*__CREATION_DES_CLES__*/
