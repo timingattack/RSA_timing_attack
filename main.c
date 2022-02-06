bdc25cd39ae9e9c560f4fc951d263d6d75454723
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-06 14:40:06 +0100

    chiffrement message / déchiffrement chiffré

--- main.c
+++ main.c
@@ -1,36 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
-#include "square_multiply.h"
-#include "Dechiffrement.h"
-#include "Creation_des_cles.h"
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include "chiffrement.h"
+#include "dechiffrement.h"
+#include "creation_des_cles.h"
+#include "miller_rabin.h"
 
-
-
+//différentes tailles de bits prédéfinies pour p et q
+#define PRIME_LONG_SIZE 1536    //RSA 3072 bits
+#define PRIME_MEDIUM_SIZE 1024  //RSA 2048 bits
+#define PRIME_LOW_SIZE 512      //RSA 1024 bits
+//nombre de bits qui seront utilisés pour p et q
+#define PRIME_NUMBER_SIZE 512
+ 
 int main(int argc, char const *argv[])
 {    
-    unsigned int p , q ,m, c;
+    mpz_t  m, e, p , q, n, c, phi_n, d;
+    mpz_init(m);        //clair
+    mpz_init(e);        //exposant publique e : 1 < e < phi(n)
+    mpz_init(p);        //nombre premier, premier facteur de n
+    mpz_init(q);        //nombre premier, deuxième facteur de n
+    mpz_init(n);        //produit des deux nombres premiers
+    mpz_init(c);        //chiffré
+    mpz_init(phi_n);    //phi(n) : (p - 1) * (q - 1)
+    mpz_init(d);        //exposant privé : e^-1 mod ph(n)
     
+    srandom(getpid() + time(NULL));
+
+    //message clair aléatoire
+    mpz_set_ui(m, (unsigned long int) random());
+    gmp_printf("\nmessage d'origine : %Zd\n\n", m);
+
+    //génération de n, p, q
+    generer_un_nombre_premier(p, PRIME_NUMBER_SIZE);
+    generer_un_nombre_premier(q, PRIME_NUMBER_SIZE);
+    mpz_mul(n, p, q);
     
-    mpz_t x, e, n,y;
-
-    mpz_init(x);
-    mpz_init(y);
-    mpz_init(e);
-    mpz_init(n);
-
-    mpz_set_ui(x, 100);
-
-    mpz_set_ui(n, generer_cle_publique_N(p,q));
-    mpz_set_ui(e, generer_cle_publique_e(p,q));
-   
+    //génération de phi de n
+    phi(p, q, phi_n);
     
-    c = square_and_multiply(x, e, n);
+    //génération de e
+    generer_exposant_public(phi_n, e);
+    gmp_printf("exposant publique e : %Zd\n", e);
 
-    mpz_set_ui(x, c);
+    chiffrement_RSA(m, e, n, c);
+    gmp_printf("\nchiffré : %Zd\n", c);
+    
+    mpz_init(m);    // m = 0
+    
+    //génération de d
+    generer_exposant_privee(e, phi_n, d);
 
-    m = dechiffrement_RSA(x,51,n);
+    dechiffrement_RSA(c, d, n, m);
+    gmp_printf("\nmessage : %Zd\n", m);
 
-        
+    mpz_clear(m);
+    mpz_clear(e);
+    mpz_clear(p);
+    mpz_clear(q);
+    mpz_clear(n);
+    mpz_clear(c);
+    mpz_init(phi_n);
+    mpz_clear(d);
+
     exit(0);
 }
