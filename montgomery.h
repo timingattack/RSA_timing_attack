e1696e90169b88787a37ec777064b204451838c0
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-11 14:21:52 +0100

    Add files via upload

---
+++ montgomery.h
@@ -0,0 +1,7 @@
+#ifndef __MONTGOMERY__
+#define __MONTGOMERY__
+#include <gmp.h>
+
+void bezout(mpz_t r, mpz_t n, mpz_t u, mpz_t v, mpz_t pgcd, mpz_t x);
+void montgomery_essai(mpz_t u, mpz_t v, mpz_t z, mpz_t r, mpz_t n,mpz_t pgcd);
+#endif /*__MONTGOMERY__*/
