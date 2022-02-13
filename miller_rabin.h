e7d2710bb1fb51773284e5897d3e835b5d330c6f
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-13 15:35:18 +0100

    suppression miller_rabin.h

--- miller_rabin.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MILLER_RABIN__
-#define __MILLER_RABIN__
-#include <gmp.h>
-#include <stdbool.h>
-
-void generer_un_nombre_premier(mpz_t p, int b);
-bool miller_rabin(const mpz_t n);
-
-#endif /*__MILLER_RABIN__*/
