7d2e96e524cab99ee55a8aaf7710445a6cf0e7aa
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-13 15:34:33 +0100

    suppression dechifrement.h

--- dechiffrement.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DECHIFFREMENT__
-#define __DECHIFFREMENT__
-#include <gmp.h>
-
-void dechiffrement_RSA(const mpz_t c, const mpz_t  d, const mpz_t n, mpz_t m);
-
-#endif /*__DECHIFFREMENT__*/
