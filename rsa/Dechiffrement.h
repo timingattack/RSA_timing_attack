8cf9476b776620e63c17234d7ae8db603667b354
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-18 21:03:10 +0100

    Delete rsa directory

--- rsa/Dechiffrement.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DECHIFFREMENT__
-#define __DECHIFFREMENT__
-#include <gmp.h>
-
-void dechiffrement_RSA( mpz_t decrypt,  mpz_t x,  unsigned int d,  mpz_t n);
-void Montgomery_Exponentiation_decrypt(mpz_t decrypt, mpz_t a, mpz_t u, mpz_t v,unsigned int d , mpz_t r, mpz_t n);
-
-#endif /*__DECHIFFREMENT__*/
