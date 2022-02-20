ea3ef195102d73afe40e8de18bb9425415b72c8b
timingattack <98774703+timingattack@users.noreply.github.com>
2022-02-20 18:05:08 +0100

    fix linux errors and warning

---
+++ temps.h
@@ -0,0 +1,18 @@
+#ifndef _TEMPS_H_
+#define _TEMPS_H_
+
+#define _GNU_SOURCE	//fix warning implicit declaration
+
+#include <sys/time.h>
+#include <time.h>
+
+struct timespec;	//fix warning scope
+
+void temps_reel(struct timespec *t);
+void temps_cpu(clock_t *t);
+void afficher_temps_reel(double *tt, struct timespec td, struct timespec tf);
+void afficher_temps_cpu(double *tt, clock_t td, clock_t tf);
+void debut_chrono(clock_t *tc, struct timespec *tr);
+void fin_chrono(double *ttc, clock_t tdc, clock_t tfc, double *ttr, struct timespec tdr, struct timespec tfr);
+
+#endif /* _TEMPS_H_ */
