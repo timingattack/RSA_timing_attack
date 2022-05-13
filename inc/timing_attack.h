7c6b913993b6a160b03d6dbf72d550577d38f533
timingattack <98774703+timingattack@users.noreply.github.com>
2022-05-13 16:06:34 +0200

    Redéfinition du code pour meilleur compréhension

--- inc/timing_attack.h
+++ inc/timing_attack.h
@@ -8,39 +8,42 @@ typedef struct element {
 	struct element* suiv;
 } ELEMENT; 				//représente un élément de l'ensemble A ou B.
 
-typedef struct ensemble {
+typedef struct liste {
 	ELEMENT* elem;
 	ELEMENT* fin;
 	unsigned long int taille;
-} ENSEMBLE; 			//représente l'ensemble A ou B (pour un bit) sous la forme d'une simple liste chaînée.
+	long double temps_moyen;
+} LISTE; 				//représente la liste des mesures (pour un bit) sous la forme d'une simple liste chaînée.
 
-typedef struct ensemble_global {
-	ENSEMBLE** bit;
-} ENSEMBLE_G;			//représente l'ensemble A et B (pour tous les bits) sous la forme d'un tableau de listes simplements chaînées.
-						//tableau de taille n, liste de taille >= nombre d'itération
-						//l'indice du tableau représente le bit, la liste représente les temps mesurés
+typedef struct ensemble {
+	LISTE** bit;
+} ENSEMBLE;				//représente l'ensemble A et B (pour tous les bits) sous la forme d'un tableau de listes chaînées.
+						//	- tableau de taille n
+						//	- liste de taille >= nombre d'itération sur le bit i
+						//	- l'indice du tableau représente le bit
+						//	- la liste représente les temps mesurés
 
 //Fonctions pour les éléments
 ELEMENT* initialiser_element(const double temps);
 void afficher_element(const ELEMENT* elem, const char* nom);
 
 //Fonctions pour les ensembles
-ENSEMBLE* initialiser_ensemble();
-void ajouter_element(ELEMENT* elem, ENSEMBLE** ens);
-ELEMENT* retourner_element(ENSEMBLE** ens);
-void afficher_ensemble(const ENSEMBLE* ens, const char* nom);
-void supprimer_ensemble(ENSEMBLE** ens, const char* nom);
+LISTE* initialiser_lsite();
+void ajouter_element_liste(ELEMENT* elem, LISTE** ens);
+ELEMENT* retourner_element_liste(LISTE** ens);
+void afficher_liste(const LISTE* ens, const char* nom);
+void supprimer_liste(LISTE** ens, const char* nom);
 
 //Fonctions pour les ensembles globaux
-ENSEMBLE_G* initialiser_ensemble_global();
-void ajouter_element_global(ELEMENT *elem, ENSEMBLE_G** eg, const unsigned int bit);
-ELEMENT* retourner_element_global(ENSEMBLE_G** eg, const unsigned int bit);
-void supprimer_ensemble_global(ENSEMBLE_G** eg, const char* nom);
-void afficher_ensemble_global(ENSEMBLE_G* eg, const char* nom);
+ENSEMBLE* initialiser_ensemble();
+void ajouter_element(ELEMENT *elem, ENSEMBLE** eg, const unsigned int bit);
+ELEMENT* retourner_element(ENSEMBLE** eg, const unsigned int bit);
+void supprimer_ensemble(ENSEMBLE** eg, const char* nom);
+void afficher_ensemble(ENSEMBLE* eg, const char* nom);
 
 //Variables globales
-extern ENSEMBLE_G* A;
-extern ENSEMBLE_G* B;
+extern ENSEMBLE* A;
+extern ENSEMBLE* B;
 extern unsigned int target_bit;					//le bit mesuré lors du timing attack
 extern unsigned int TIMING_ATTACK_CONFIRMED;	//0 = désactive le timing attack, 1 = active le timing attack
 
