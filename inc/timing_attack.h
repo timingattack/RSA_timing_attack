216a00ec188bba1a98deaa3bafbcd32f825445b1
timingattack <98774703+timingattack@users.noreply.github.com>
2022-05-14 10:20:08 +0200

    Calcul de la différence des temps moyen (pas fini)

--- inc/timing_attack.h
+++ inc/timing_attack.h
@@ -1,7 +1,8 @@
 #ifndef _TIMING_ATTACK_H_
 #define _TIMING_ATTACK_H_
 
-#define EPSILON 4e-6	//borne de répartition des mesures
+#define LIMITE 4e-6	//borne de répartition des mesures
+#define EPSILON 1e-6
 
 typedef struct element {
 	double temps;
@@ -14,6 +15,7 @@ typedef struct liste {
 	unsigned long int taille;
 	double temps_moyen;
 	double temps_total;
+	double difference_temps_moyen;
 } LISTE; 				//représente la liste des mesures (pour un bit) sous la forme d'une simple liste chaînée.
 
 typedef struct ensemble {
@@ -23,6 +25,11 @@ typedef struct ensemble {
 						//	- liste de taille >= nombre d'itération sur le bit i
 						//	- l'indice du tableau représente le bit
 						//	- la liste représente les temps mesurés
+
+typedef struct tab {
+	double* difference;
+	unsigned int* bit_value;
+} TAB;					//représente une map des différences des temps moyens entre A et B et la valeur du bit correspondante.
 
 //Fonctions pour les éléments
 ELEMENT* initialiser_element(const double temps);
@@ -46,15 +53,23 @@ void afficher_ensemble_complet(ENSEMBLE* eg, const char* nom);
 void afficher_ensemble_simple(ENSEMBLE* eg, const char* nom);
 void calculer_temps_moyen(ENSEMBLE** ens);
 
+//Fonctions pour la map
+TAB* initialiser_tableau();
+void supprimer_tableau(TAB** tab);
+void afficher_tableau_T();
+
 //Variables globales
 extern ENSEMBLE* A;
 extern ENSEMBLE* B;
+extern TAB* T;
 extern unsigned int target_bit;					//le bit mesuré lors du timing attack
 extern unsigned int TIMING_ATTACK_CONFIRMED;	//0 = désactive le timing attack, 1 = active le timing attack
 
 //Fonctions pour les variables globales
 void initialiser_variables_globales_timing_attack();
 
-void test(); 			//fonction de test pour tester les fonctions (à faire: à retirer lorsque timing attack fini)
+//Fonctions générales
+void calculer_difference_temps_moyen(ENSEMBLE** a, ENSEMBLE** b);	//calcul la différence des temps moyens entre a et b
+void test(); 		//fonction de test pour tester les fonctions (à faire: à retirer lorsque timing attack fini)
 
 #endif /* _TIMING_ATTACK_H_ */
