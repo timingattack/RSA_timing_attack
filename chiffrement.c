02f23ab3a12ebc3345abddd371547ecb47401f2d
timingattack <98774703+timingattack@users.noreply.github.com>
2022-05-14 16:45:06 +0200

    d reconstituer sauf le premier bit dk-1

---
+++ chiffrement.c
@@ -0,0 +1,444 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gmp.h>
+#include <stdbool.h>
+#include <math.h>
+#include <unistd.h>        //pour faciliter le timing attack (à faire: à retirer lorsque le timing attack sera fini)
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include "miller_rabin.h"
+#include "creation_des_cles.h"
+#include "square_multiply.h"
+#include "chiffrement.h"
+#include "temps.h"
+#include "timing_attack.h"
+
+#define PADDING_SIZE 88
+#define BYTE_SIZE 8
+
+/* PKCS#1 v1.5 pour le chiffrement
+* - de la forme : 00 02 ALEA 00 | M
+* - taille :        (88 bits)   | (variable)
+*/
+static void padding_chiffrement(mpz_t m)
+{
+   size_t i, j;
+   double bit_value = 0, limit_value = 0;
+   char* random_number = malloc(sizeof(char) * PADDING_SIZE + mpz_sizeinbase(m, 2) + 1); //chaine de 88 bits (11 octets) + bits du message
+   char* byte = malloc(sizeof(char) * BYTE_SIZE + 1);   //chaine de 8 bits (1 octet)
+   char* tmp = malloc(mpz_sizeinbase(m,2) + 1);
+   
+   if(!random_number || !byte || !tmp)
+   {
+      fprintf(stderr, "Erreur: échec du malloc pour le padding chiffrement.\n");
+      free(random_number);
+      free(byte);
+      free(tmp);
+      exit(7);
+   }
+
+   random_number[0] = '\0';
+   byte[0] = '\0';
+   tmp[0] = '\0';
+
+   for(i = 0; i < 12; i++)
+   {
+      //message
+      if(i == 11)
+      {
+         mpz_get_str(tmp, 2, m);
+         size_t taille_message = strlen(tmp);
+
+         for(j = 0; j <= taille_message; j++)
+         {
+            strncat(random_number, &tmp[j], 1);
+         }
+
+         mpz_set_str(m, random_number, 2);
+         //gmp_printf("PKCS#1 v1.5 chiffré: %Z0X\n", m);
+         break;
+      }
+      //1 octet ØØ
+      if(i == 10)
+      {
+         for(j = 0; j < BYTE_SIZE; j++)
+         {  
+            strncat(random_number, "0", 1);
+         }
+         continue;
+      }
+      //8 octets aléatoires différents de 00
+      if(1 < i && i < 10)
+      {
+         do 
+         {
+            strncpy(byte,"\0", strlen(byte));
+            for(j = 0; j < BYTE_SIZE; j++)
+            {
+               random_bit(&byte, bit_value, limit_value);
+            }
+         } while(strncmp(byte,"00000000", BYTE_SIZE) == 0);
+
+         strncat(random_number, byte, BYTE_SIZE);
+         continue;
+      }
+      //#1 octet Ø2
+      if(i == 1)
+      {
+         strncat(random_number, "00000010", BYTE_SIZE);
+         continue;
+      }
+      //#1 octet ØØ
+      if(i == 0)
+         strncat(random_number, "00000000", BYTE_SIZE);
+   }
+
+   free(random_number);
+   free(byte);
+   free(tmp);
+}
+
+void chiffrement_RSA(mpz_t m, const mpz_t e, const mpz_t n, mpz_t c)
+{
+   if(padding)
+      padding_chiffrement(m);
+   square_and_multiply(m, e, n, c);
+}
+
+//SHA256 hash
+void hash(mpz_t hm)
+{
+   unsigned int i = 0;
+   size_t taille = 0;   
+   EVP_MD_CTX* ctx = NULL;          //message digest context
+   EVP_MD* sha256 = NULL;           //message digest
+   unsigned int error = 1, len = 0;
+   unsigned char* outdigest = NULL;
+   char* msg = malloc(mpz_sizeinbase(hm, 16) + 1);
+   char* tmp = malloc(65);          //32 blocs de SHA256
+   char* hexa = malloc(3);
+   
+   msg[0] = '\0';
+   tmp[0] = '\0';
+   hexa[0] = '\0';
+   mpz_get_str(msg, 16, hm);        //recupération du message en hexa
+
+   // Create a context for the digest operation
+   ctx = EVP_MD_CTX_new();
+   if (ctx == NULL)
+      goto err;
+
+   /*
+   * Fetch the SHA256 algorithm implementation for doing the digest. We're
+   * using the "default" library context here (first NULL parameter), and
+   * we're not supplying any particular search criteria for our SHA256
+   * implementation (second NULL parameter). Any SHA256 implementation will
+   * do.
+   */
+   sha256 = EVP_MD_fetch(NULL, "SHA256", NULL);
+   if(sha256 == NULL)
+      goto err;
+
+   // Initialise the digest operation
+   if(!EVP_DigestInit_ex(ctx, sha256, NULL))
+      goto err;
+
+   /*
+   * Pass the message to be digested. This can be passed in over multiple
+   * EVP_DigestUpdate calls if necessary
+   */
+   if(!EVP_DigestUpdate(ctx, msg, sizeof(msg)))
+      goto err;
+
+   // Allocate the output buffer
+   outdigest = OPENSSL_malloc(EVP_MD_get_size(sha256));
+   if(outdigest == NULL)
+      goto err;
+
+   // Now calculate the digest itself
+   if(!EVP_DigestFinal_ex(ctx, outdigest, &len))
+      goto err;
+
+   // Print out the digest result
+   //BIO_dump_fp(stdout, outdigest, len);
+   
+   for(i = 0; i < 32; i++)
+   {
+      sprintf(hexa, "%x", outdigest[i]);
+      taille = sizeof(hexa);
+      if(strcmp(hexa,"0") == 0)
+      {
+         strncat(tmp, "00", 2);
+      }
+      else if((taille == 1) && strcmp(hexa,"0") != 0)
+      {
+         strncat(tmp, "0", 1);
+         strncat(tmp, hexa, 1);
+      }
+      else
+      {
+         strncat(tmp, hexa, taille);
+      }
+      //printf("hexa: %s\n", hexa);
+      //printf("hash: %s\n", tmp);
+   }
+   mpz_set_str(hm, tmp, 16);   //récupération du hash
+
+   error = 0;
+
+   err:
+      // Clean up all the resources we allocated
+      OPENSSL_free(outdigest);
+      EVP_MD_free(sha256);
+      EVP_MD_CTX_free(ctx);
+      free(msg);
+      free(tmp);
+      free(hexa);
+      if(error != 0)
+         ERR_print_errors_fp(stderr);
+}
+
+/* PKCS#1 v1.5 pour la signature
+* - de la forme : 00 01 FF FF FF FF FF FF FF FF 00 | H(M)
+* - taille :                  (88 bits)            | (variable)
+*/
+void padding_signature(mpz_t m)    //m est déjà hashé
+{
+   size_t i, j;
+   char* random_number = malloc(sizeof(char) * PADDING_SIZE + mpz_sizeinbase(m, 2) + 1); //chaine de 88 bits (11 octets) + bits du message
+   char* byte = malloc(sizeof(char) * BYTE_SIZE + 1);   //1 octet
+   char* tmp = malloc(mpz_sizeinbase(m, 2) + 1);
+   
+   if(!random_number || !byte || !tmp)
+   {
+      fprintf(stderr, "Erreur: échec du malloc pour le padding signature.\n");
+      free(random_number);
+      free(byte);
+      free(tmp);
+      exit(9);
+   }
+
+   random_number[0] = '\0';
+   byte[0] = '\0';
+   tmp[0] = '\0';
+
+   for(i = 0; i < 12; i++)
+   {
+      //message
+      if(i == 11)
+      {
+         mpz_get_str(tmp, 16, m);       
+         size_t taille_message = strlen(tmp);
+
+         for(j = 0; j <= taille_message; j++)
+         {
+            strncat(random_number, &tmp[j], 1);
+         }
+
+         mpz_set_str(m, random_number, 16);
+
+         //gmp_printf("PKCS#1 v1.5 signé: %Z0X\n", m);
+         break;
+      }
+      //1 octet ØØ
+      if(i == 10)
+      {
+         strncat(random_number, "00", 2);
+         continue;
+      }
+      //8 octets FF
+      if(1 < i && i < 10)
+      {
+         strncat(random_number, "FF", 2);
+         continue;
+      }
+      //#1 octet Ø1
+      if(i == 1)
+      {
+         strncat(random_number, "01", 2);
+         continue;
+      }
+      //#1 octet ØØ
+      if(i == 0)
+         strncat(random_number, "00", 2);
+   }
+
+   free(random_number);
+   free(byte);
+   free(tmp);
+}
+
+// signe le message lors du chiffrement
+void signature(mpz_t m, const mpz_t d, const mpz_t n, mpz_t s)
+{
+   if(padding)
+      padding_signature(m);
+   square_and_multiply(m, d, n, s);
+}
+
+//--------------------------------------MONTGOMERY-------------------------------------------//
+
+/* Generateur de r =  2^1024 ( RSA 1024 bits ) */
+void generer_R_montgomery(mpz_t r)
+{
+      mpz_ui_pow_ui(r, 2, n_size);
+}
+
+/* Applique le theoreme de Bezout sur n et r et renvoie u et v tel que :
+*                 - RU + NV = 1
+*/
+void bezout(const mpz_t r, const mpz_t n , mpz_t u, mpz_t v, mpz_t pgcd)
+{
+   if(!mpz_cmp_ui(n, 0))
+   {
+      mpz_set(pgcd, r);
+      mpz_set_ui(u, 1);
+      mpz_init(v);
+   }
+   else
+   {
+      mpz_t q, reste, u1, v1, v_q;
+      mpz_inits(q, reste, u1, v1, v_q, NULL);
+      
+      mpz_tdiv_qr(q, reste, r, n);
+      bezout(n, reste, u1, v1, pgcd); 
+      mpz_set(u, v1);
+      mpz_mul(v_q, v1, q);
+      mpz_sub(v, u1, v_q);
+
+      mpz_clears(q, reste, u1, v1, v_q, NULL);
+   }
+}
+
+void Montgomery_product(const mpz_t v, const mpz_t a_bar, const mpz_t b_bar, const mpz_t n, mpz_t t, const unsigned int N_SIZE)
+{ 
+   mpz_t m, z, nprim; 
+   mpz_inits(nprim,m,z,NULL);
+
+   mpz_neg(nprim,v); // nprim = -v
+   //MONTGOMERY
+   /*
+   * m = ( z mod r ) * nprim mod r
+   * t = ( z + m * n ) / r
+   * si t > n --> t = t - n
+   * sinon  t
+   */
+   mpz_mul(z,a_bar,b_bar); // z = abar bbar
+   mpz_mod_2exp(m,z, N_SIZE); // m = ( z mod r )
+   mpz_mul(m,m,nprim); //  m = ( z mod r ) * nprim
+   mpz_mod_2exp(m,m, N_SIZE); // m = ( z mod r ) * nprim mod r
+   mpz_mul(t,m,n ); // t = ( m * n )
+   mpz_add(t,z,t); // t = ( z + m * n )
+   mpz_tdiv_q_2exp(t,t, N_SIZE); // t = ( z + m * n ) / r
+
+   //###########################-TIMING ATTACK-#################################//
+   double tta = 0.0, tta_cpu = 0.0;
+   clock_t tta_cpu_deb = 0, tta_cpu_fin = 0;
+   struct timespec tta_deb = {0,0}, tta_fin = {0,0};
+
+   struct timespec time_if = {0,4000};
+
+   if(TIMING_ATTACK_CONFIRMED)
+      debut_chrono(&tta_cpu_deb,&tta_deb);
+      //debut_chrono_timing_attack(&tta_deb);
+   //###########################################################################//
+   
+   //int in_if = 0;
+   
+   if((mpz_cmp(t, n) == 0) || (mpz_cmp(t,n) > 0)) // t >= n
+   {
+      mpz_sub(t, t, n); // t = t - n
+
+      nanosleep(&time_if, NULL);
+      //in_if = 1;
+   }
+   //printf("bit_position = %u\n", bit_position);
+   //###########################-TIMING ATTACK-#################################//
+   //if(bit_cible == bit_position)
+   //{
+   if(TIMING_ATTACK_CONFIRMED)
+   {
+      //printf("target_bit %d\n", target_bit);
+      fin_chrono(&tta_cpu,tta_cpu_deb,tta_cpu_fin,&tta,tta_deb,tta_fin);
+      //printf("\n");
+      
+      //fin_chrono_timing_attack(&tta, tta_deb, tta_fin);
+      
+      ELEMENT* elem = initialiser_element(tta_cpu);
+      
+      /*if(in_if){
+         printf("temps = %ld\n", test.tv_nsec);
+         afficher_element(elem, "elem");
+      }*/
+      if(elem->temps <= LIMITE) //si le if n'a pas ete fait et le sleep n'a pas ete fait
+         ajouter_element(elem, &A, target_bit);
+      else
+         ajouter_element(elem, &B, target_bit);
+
+      calculer_temps_moyen(&A);  //calcul Ta
+      calculer_temps_moyen(&B);  //calcul Tb
+
+      calculer_difference_temps_moyen(&A, &B);  //met le résultat dans le tableau T
+   }
+   //}
+   //###########################################################################//
+
+   mpz_clear(m);
+   mpz_clear(nprim);
+   mpz_clear(z);
+}
+
+void Montgomery_Exponentiation_crypt(mpz_t crypt, const mpz_t a, const mpz_t v, const mpz_t e, const mpz_t n, const unsigned int N_SIZE)
+{
+   unsigned int k, taille;
+   taille = mpz_sizeinbase(e, 2);
+   mpz_t a_bar, x_bar, rop1, un, rshiftr, andr , msk;
+   mpz_inits(a_bar, x_bar, rop1, un, rshiftr, andr, msk, NULL);
+   mpz_set_ui(un, 1);
+   mpz_set_ui(msk, 1);
+
+   mpz_mul_2exp(rop1, a, N_SIZE); // rop1 = a * r (r = 2^N_SIZE)
+   mpz_mod(a_bar, rop1, n); // a_bar = ( a * r ) mod n
+   mpz_mul_2exp(x_bar, un, N_SIZE); // x_bar = 1 * r (r = 2^N_SIZE) 
+  
+   for(k = taille; k > 0; k--)
+   {
+      
+      //###########################################################################//
+      
+
+      Montgomery_product(v, x_bar, x_bar, n, x_bar, N_SIZE); // square 
+      mpz_tdiv_q_2exp(rshiftr, e, k - 1);
+      mpz_and(andr, rshiftr, msk);
+
+      if(!(mpz_cmp_ui(andr, 1)))
+      {  
+             //###########################-TIMING ATTACK-#################################//
+      if(k <= taille - 1 && TIMING_ATTACK_CONFIRMED)  //on commence a dk-2
+      {
+         target_bit = k - 1;
+      }
+         Montgomery_product(v, a_bar, x_bar, n, x_bar, N_SIZE); // multiply 
+      }
+   }
+
+   TIMING_ATTACK_CONFIRMED = 0;  //désactive le timing attack
+ 
+   //Montgomery_product(v, x_bar, un, n, crypt, N_SIZE); // calcul du chiffre
+
+   mpz_clears(a_bar, x_bar, rop1, un, rshiftr, andr, msk, NULL);
+}
+
+void verification(const mpz_t u, const mpz_t z, const mpz_t n, mpz_t verif)
+{
+   mpz_mul(verif, z, u); mpz_mod(verif, verif, n);
+}
+
+void chiffrement_RSA_montgomery(mpz_t m, const mpz_t e, const mpz_t n, mpz_t c, const mpz_t v, const unsigned int N_SIZE)
+{
+   if(padding)
+      padding_chiffrement(m);
+   Montgomery_Exponentiation_crypt(c, m, v, e, n, N_SIZE);
+}
---
+++ rsa.c
@@ -0,0 +1,325 @@
+#define _GNU_SOURCE //fix timespec initialisation
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <gmp.h>
+#include "temps.h"
+#include <time.h>
+#include <sys/time.h>
+#include "creation_des_cles.h"
+#include "chiffrement.h"
+#include "dechiffrement.h"
+#include "rsa.h"
+#include "timing_attack.h"
+
+static void choix_mode_rsa(char* choix)
+{
+    int ctr = 1;
+    printf("\n\tChoisir le mode de RSA :\n\t- normal (1)\n\t- montgomery (2)\n\t  ");
+    *choix = getchar();
+    while(getchar() != '\n')
+        ctr++;
+
+    if((*choix != '1' && *choix != '2') || ctr != 1)
+    {
+        fprintf(stderr, "Erreur : saisie incorrecte\n");
+        printf("Usage : entrez la touche 1 ou 2\n");
+        choix_mode_rsa(choix);
+    }
+}
+
+static void choix_taille_module_rsa(char* choix)
+{
+    int ctr = 1;
+    printf("\n\tChoisir la taille du module RSA :\n\t- petite : 1024 (1)\n\t- moyenne : 2048 (2)\n\t- grande : 3072 (3)\n\t- très grande : 4096 (4)\n\t  ");
+    *choix = getchar();
+    while(getchar() != '\n')
+        ctr++;
+
+    if((*choix != '1' && *choix != '2' && *choix != '3' && *choix != '4') || ctr != 1)
+    {
+        fprintf(stderr, "Erreur : saisie incorrecte\n");
+        printf("Usage : entrez la touche 1, 2, 3 ou 4\n");
+        choix_taille_module_rsa(choix);
+    }
+}
+
+//A faire: à supprimer lorsque l'implémentation de la condition d'arrêt de l'attaque temporelle sera finie
+static void choix_loop_iteration(char*  choix, char** iter)
+{
+    unsigned int taille = 1, str_length;
+    char* tmp = NULL;
+    printf("\n\tChoisir le nombre iteration pour le timing attack :\n\t  ");
+    *choix = getchar();
+    if(*choix != '0' && *choix != '1' && *choix != '2' && *choix != '3' && *choix != '4' && *choix != '5' && *choix != '6' && *choix != '7' && *choix != '8' && *choix != '9')
+    {
+        while(getchar() != '\n');
+        choix_loop_iteration(choix, iter);
+        return;
+    }
+    *iter = malloc(sizeof(char) + 1);
+    strncpy(*iter, choix, 1);
+    while(1)
+    {
+        *choix = getchar();
+        if(*choix == '\n')
+            return;
+        if(*choix != '0' && *choix != '1' && *choix != '2' && *choix != '3' && *choix != '4' && *choix != '5' && *choix != '6' && *choix != '7' && *choix != '8' && *choix != '9')
+        {
+            fprintf(stderr, "Erreur : saisie incorrecte\n");
+            printf("Usage : entrez un nombre\n");
+            if(tmp)
+                free(tmp);
+            free(*iter);
+            choix_loop_iteration(choix, iter);
+            return;
+        }
+        taille++;
+        if(tmp)
+            free(tmp);
+        tmp = malloc(sizeof(char) * strlen(*iter) + 1);
+        str_length = sizeof(*iter);
+        strncpy(tmp, *iter, str_length);
+        free(*iter);
+        *iter = malloc(sizeof(char) * taille + 1);
+        str_length = sizeof(tmp);
+        strncpy(*iter, tmp, str_length);
+        strncat(*iter, choix, 1);
+    }
+}
+
+static void choix_padding(char* choix)
+{
+    int ctr = 1;
+    printf("\n\tAvec padding PKCS#1 v1.5 ?\n\t- oui (1)\n\t- non (2)\n\t  ");
+    *choix = getchar();
+    while(getchar() != '\n')
+        ctr++;
+
+    if((*choix != '1' && *choix != '2') || ctr != 1)
+    {
+        fprintf(stderr, "Erreur : saisie incorrecte\n");
+        printf("Usage : entrez la touche 1 ou 2\n");
+        choix_padding(choix);
+    }
+}
+
+void run()
+{
+    //unsigned long int i;
+    unsigned long int nombre_iteration;
+    char m = '\0', t = '\0', l = '\0', p = '\0';
+    char* iteration = NULL;
+    srand(getpid() + time(NULL));           //utiliser pour p, q, PKCS#1, (et le message clair)
+    printf("\n\t\t--Timing Attack--\n");
+    choix_mode_rsa(&m);
+    choix_taille_module_rsa(&t);
+    choix_loop_iteration(&l,&iteration);    //A faire: (voir le commentaire au début de la fonction)
+    choix_padding(&p);
+
+    if(t == '1')
+    {
+        printf("\n\t\tRSA-1024");
+        prime_size = PRIME_LOW_SIZE;
+    }
+
+    if(t == '2')
+    {
+        printf("\n\t\tRSA-2048");
+        prime_size = PRIME_MEDIUM_SIZE;
+    }
+
+    if(t == '3')
+    {
+        printf("\n\t\tRSA-3072");
+        prime_size = PRIME_LONG_SIZE;
+    }
+
+    if(t == '4')
+    {
+        printf("\n\t\tRSA-4096");
+        prime_size = PRIME_VERY_LONG_SIZE;
+    }
+
+    if(m == '1')
+        printf("-CLASSIQUE");
+    else
+        printf("-MONTGOMERY");
+
+    if(p == '1'){
+        padding = true;
+        printf("-AVEC_PADDING\n\n");
+    }
+    else{
+        padding = false;
+        printf("-SANS_PADDING\n\n");
+    }
+
+    nombre_iteration = atoi(iteration);
+
+    //for(i = 0; i < nombre_iteration; i++)
+        run_rsa(m, nombre_iteration);
+}
+
+void run_rsa(const char mode, unsigned long int numero_iteration)
+{
+    //--------------------CHRONO--------------------------
+    clock_t t_cpu_deb = 0, t_cpu_fin = 0;
+    struct timespec t_reel_deb = {0,0}, t_reel_fin = {0,0};
+    double temps_reel_total = 0.0, temps_cpu_total = 0.0;
+    debut_chrono(&t_cpu_deb,&t_reel_deb);
+    //----------------------------------------------------
+    
+    //unsigned int i;
+    unsigned long int iteration = numero_iteration;
+    mpz_t  m, e, p , q, n, c, phi_n, d, pkcs_sgn, s, hm, r, u, v, pgcd_bezout;
+    mpz_init(m);        //clair
+    mpz_init(e);        //exposant publique e : 1 < e < phi(n)
+    mpz_init(p);        //nombre premier, premier facteur de n
+    mpz_init(q);        //nombre premier, deuxième facteur de n
+    mpz_init(n);        //produit des deux nombres premiers
+    mpz_init(c);        //chiffré
+    mpz_init(phi_n);    //phi(n) : (p - 1) * (q - 1)
+    mpz_init(d);        //exposant privé : e^-1 mod ph(n)
+    mpz_init(pkcs_sgn); //PKCS#1 signature : µ(m)
+    mpz_init(s);        //signature : µ(m)^(d) mod n
+    mpz_init(hm);       //hash du message pour PKCS#1 signature : H(m)
+    mpz_init(r);
+    mpz_init(u);
+    mpz_init(v);
+    mpz_init(pgcd_bezout);
+
+    printf("chiffrement n°%lu\n", iteration - numero_iteration + 1);
+    //message clair aléatoire
+    mpz_set_ui(m, (unsigned long int) rand());
+    gmp_printf("message d'origine : %Z0X\n", m);
+    mpz_set(s, m);
+    mpz_set(hm, m);
+    
+    if(padding)
+        hash(hm);
+    
+    //gmp_printf("\nhash : %Z0X\n", hm);
+    mpz_set(pkcs_sgn, hm);
+
+    //génération de n, p, q
+    generer_npq(n, p, q);
+    gmp_printf("\np : %Zd, \nq : %Zd, \nn : %Zd\n", p, q, n);
+    
+    //génération de phi(n)
+    phi(p, q, phi_n);
+    gmp_printf("\nphi(n) : %Zd\n", phi_n);
+    
+    //génération de e
+    generer_exposant_public(e);
+    gmp_printf("\nexposant publique e : %Zd\n", e);
+
+    //génération de d
+    generer_exposant_privee(e, phi_n, d);
+    gmp_printf("\nexposant privee d : %Zd\n", d);
+
+    unsigned int taille = mpz_sizeinbase(d, 2); /**/
+
+    //###########################-TIMING ATTACK-#################################//
+
+    initialiser_variables_globales_timing_attack();
+
+    //for(bit_cible = n_size; bit_cible > 0; bit_cible--)     //le bit ciblé lors du timing attack
+    //{
+        while(numero_iteration)
+        {
+            if(iteration != numero_iteration)
+            {
+                printf("chiffrement n°%lu\n", iteration - numero_iteration + 1);
+                mpz_set_ui(m, (unsigned long int) rand());  //génération d'un nouveau message aléatoire
+                gmp_printf("message d'origine : %Z0X\n", m);
+                mpz_set(s, m);
+                mpz_set(hm, m);
+                hash(hm);
+                mpz_set(pkcs_sgn, hm);
+            }
+
+            if(mode == '2')
+            {
+                //générer R
+                generer_R_montgomery(r);
+                //bezout
+                bezout(r, n, u, v, pgcd_bezout);
+                //gmp_printf("R(%Zd) * v(%Zd) + N(%Zd) * u(%Zd)\n\n", r, u, n, v);
+            }
+
+            //chiffrement de m
+            if(mode == '1')
+                chiffrement_RSA(m, e, n, c);
+            else
+                chiffrement_RSA_montgomery(m, e, n, c, v, n_size);
+            gmp_printf("chiffré : %Z0X\n", c);
+
+            //signature de m
+            signature(pkcs_sgn, d, n, s);
+            //gmp_printf("\nsignature : %Z0X\n", s);
+
+            //vérification de s
+            verification_signature(s, e, n, hm);
+
+            TIMING_ATTACK_CONFIRMED = 1;   //active le timing attack
+            
+            //dechifrement de m
+            if(mode == '1')
+                dechiffrement_RSA(c, d, n, m);
+            else
+                dechiffrement_RSA_montgomery(c, d, n, m, v, n_size);
+            gmp_printf("message déchiffré : %Z0X\n", m);
+            
+            numero_iteration--;
+            printf("\n");
+        }
+
+        afficher_ensemble_simple(A, "A");
+        printf("\n");
+        afficher_ensemble_simple(B, "B");
+
+        afficher_tableau_T();
+
+        //numero_iteration = iteration;
+
+        printf("\n\n\n");
+    //}
+
+    printf("taille de d : %u\n", taille);
+    affichage_binaire_mpz(d);
+    printf("taille de n : %u\n", n_size);
+    gmp_printf("\nd : %Zd\n", d);
+    printf("\n");
+
+    //désallocation des ensembles A et B
+    supprimer_ensemble(&A, "A");
+    supprimer_ensemble(&B, "B");
+    //désallocation du tableau T
+    supprimer_tableau(&T);
+
+    //############################-TIMING ATTACK-################################//
+
+    mpz_clear(m);
+    mpz_clear(e);
+    mpz_clear(p);
+    mpz_clear(q);
+    mpz_clear(n);
+    mpz_clear(c);
+    mpz_clear(phi_n);
+    mpz_clear(d);
+    mpz_clear(pkcs_sgn);
+    mpz_clear(s);
+    mpz_clear(hm);
+    mpz_clear(r);
+    mpz_clear(u);
+    mpz_clear(v);
+    mpz_clear(pgcd_bezout);
+
+    //-----------------------------------FIN_CHRONO-----------------------------------------
+    fin_chrono(&temps_cpu_total,t_cpu_deb,t_cpu_fin,&temps_reel_total,t_reel_deb,t_reel_fin);
+    printf("\n");
+}
